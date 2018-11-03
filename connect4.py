08651fc7d9e48d1cc47594959e312c1649c92163
Zi Kai <ngzikai92@gmail.com>
2018-11-03 23:25:43 +0800

    Commit before logic change to check win

--- connect4.py
+++ connect4.py
@@ -2,22 +2,75 @@
 import numpy as np
 
 class Game:
-    def __init__(self):
-        self.mat = None
-        self.row = 0
-        self.col = 0
+    def __init__(self, rows, cols):
+        self.mat = np.zeros((rows, cols))
+        self.row = rows
+        self.col = cols
         self.turn = 0
         self.wins = 0
 
-    #Creates game board based on rows and cols
-    def create_board(self):
-        self.mat = np.zeros((self.row, self.col))
-
 def check_victory(game):
-    pass
+    board = game.mat
+
+    # check horizontal spaces for human player
+    for y in range(game.row):
+        for x in range(game.col - 3):
+            if board[x][y] == 1 and board[x+1][y] == 1 and board[x+2][y] == 1 and board[x+3][y] == 1:
+                return 1
+
+    # check vertical spaces for human player
+    for x in range(game.col):
+        for y in range(game.row - 3):
+            if board[x][y] == 1 and board[x][y+1] == 1 and board[x][y+2] == 1 and board[x][y+3] == 1:
+                return 1
+
+    # check / diagonal spaces for human player
+    for x in range(game.col - 3):
+        for y in range(3, game.row):
+            if board[x][y] == 1 and board[x+1][y-1] == 1 and board[x+2][y-2] == 1 and board[x+3][y-3] == 1:
+                return 1
+
+    # check \ diagonal spaces for human player
+    for x in range(game.col - 3):
+        for y in range(game.row - 3):
+            if board[x][y] == 1 and board[x+1][y+1] == 1 and board[x+2][y+2] == 1 and board[x+3][y+3] == 1:
+                return 1
+
+    # check horizontal spaces for computer player
+    for y in range(game.row):
+        for x in range(game.col - 3):
+            if board[x][y] == 2 and board[x+1][y] == 2 and board[x+2][y] == 2 and board[x+3][y] == 2:
+                return 2
+
+    # check vertical spaces for computer player
+    for x in range(game.col):
+        for y in range(game.row - 3):
+            if board[x][y] == 2 and board[x][y+1] == 2 and board[x][y+2] == 2 and board[x][y+3] == 2:
+                return 2
+
+    # check / diagonal spaces for computer player
+    for x in range(game.col - 3):
+        for y in range(3, game.row):
+            if board[x][y] == 2 and board[x+1][y-1] == 2 and board[x+2][y-2] == 2 and board[x+3][y-3] == 2:
+                return 2
+
+    # check \ diagonal spaces for computer player
+    for x in range(game.col - 3):
+        for y in range(game.row - 3):
+            if board[x][y] == 2 and board[x+1][y+1] == 2 and board[x+2][y+2] == 2 and board[x+3][y+3] == 2:
+                return 2
+
+    #board is full and neither computer nor hunans are winning
+    if is_full(game):
+        return 3
+
+    return 0
 
 def apply_move(game, col, pop):
-    pass
+    for x in range(game.row):
+        if game.mat[game.row - x-1][col] == 0:
+            game.mat[game.row - x-1][col] = pop
+            break
 
 def check_move(game, col, pop):
     pass
@@ -28,17 +81,45 @@ def computer_move(game, level):
 def display_board(game):
     print(game.mat)
 
+def is_full(game):
+    for x in range(game.row):
+        for y in range(game.col):
+            if game.mat[x][y] == 0:
+                return False
+    return True
+
 def menu():
-    pass
+    print ("Welcome to Connect Four! Please enter the number of rows and columns for your board")
+    r = input("Rows: ")
+    c = input("Cols: ")
+
+    game = Game(r, c)
+
+    # game.mat[1][3] = 1
+    # game.mat[0][0] = 2
+    # game.mat[4][5] = 3
+
+    apply_move(game, 0, 1)
+    apply_move(game, 1, 2)
+    apply_move(game, 1, 1)
+    apply_move(game, 2, 2)
+    apply_move(game, 2, 2)
+    apply_move(game, 2, 1)
+    apply_move(game, 3, 2)
+    apply_move(game, 3, 2)
+    apply_move(game, 3, 2)
+    apply_move(game, 3, 1)
 
 
+    display_board(game)
 
-myGame = Game()
-myGame.row = 5
-myGame.col = 6
+    if check_victory(game) == 1:
+        print("Human has won!")
 
-myGame.createBoard()
-print (myGame.mat)
+    print(is_full(game))
+
+if __name__ == "__main__":
+    menu()
 
 #
 # def isWinner(board, tile):
